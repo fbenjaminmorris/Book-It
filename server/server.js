02c0caba2858827d55ce74a7325e6b70a1280464
fbenjaminmorris <83195283+fbenjaminmorris@users.noreply.github.com>
2021-10-15 10:22:43 -0400

    await server.start

--- server/server.js
+++ server/server.js
@@ -16,25 +16,29 @@ const server = new ApolloServer({
   context: authMiddleware
 });
 
+async function startApolloServer() {
+  await server.start();
+  server.applyMiddleware({ app });
 
-server.applyMiddleware({ app });
-
-app.use(express.urlencoded({ extended: true }));
-app.use(express.json());
+  app.use(express.urlencoded({ extended: true }));
+  app.use(express.json());
 
 
-if (process.env.NODE_ENV === 'production') {
-  app.use(express.static(path.join(__dirname, '../client/build')));
+  if (process.env.NODE_ENV === 'production') {
+    app.use(express.static(path.join(__dirname, '../client/build')));
+  }
+
+
+  app.get('*', (req, res) => {
+    res.sendFile(path.join(__dirname, '../client/build/index.html'));
+  });
+
+  db.once('open', () => {
+    app.listen(PORT, () => {
+      console.log(`API server running on port ${PORT}!`);
+      console.log(`Use GraphQL at http://localhost:${PORT}${server.graphqlPath}`);
+    });
+  });
 }
 
-
-app.get('*', (req, res) => {
-  res.sendFile(path.join(__dirname, '../client/build/index.html'));
-});
-
-db.once('open', () => {
-  app.listen(PORT, () => {
-    console.log(`API server running on port ${PORT}!`);
-    console.log(`Use GraphQL at http://localhost:${PORT}${server.graphqlPath}`);
-  });
-});
+startApolloServer();
